733d421a86f059c9f4daa9111610d76ea7b4309f
Jacomo <jacomonh@hotmail.com>
2022-10-11 19:18:44 -0300

    change to useRef to test

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-import { useState, useRef, useEffect } from "react";
+import { useRef } from "react";
 import isEqual from "lodash.isequal";
 
 type DependencyItem = unknown;
 type ReRenderTrigger = number;
 
 const useDRR = (dependencyItem: DependencyItem): ReRenderTrigger => {
-  const [triggerRender, setTriggerRender] = useState<ReRenderTrigger>(0);
+  const triggerRender = useRef<ReRenderTrigger>(0);
   const dependencyItemRef = useRef<DependencyItem>(dependencyItem);
-  useEffect(() => {
-    if (isEqual(dependencyItem, dependencyItemRef.current)) return;
+  if (!isEqual(dependencyItem, dependencyItemRef.current)) {
     dependencyItemRef.current = dependencyItem;
-    setTriggerRender((t) => t + 1);
-  });
-  return triggerRender;
+    triggerRender.current++;
+  }
+  return triggerRender.current;
 };
 
 export default useDRR;
